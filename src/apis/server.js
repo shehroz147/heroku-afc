91f52252997a6c38baa759a6fb71b5f80f7c8c74
Shehroz Virk <shehroz.virk@gmail.com>
2021-12-22 16:35:28 +0500

    add

--- src/apis/server.js
+++ src/apis/server.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const { REACT_APP_SERVER_BASE_URL } = process.env;
-export const baseUrl = "https://hamza-flawss.herokuapp.com";
+export const baseUrl = "https://afchalal.herokuapp.com";
 // export const baseUrl = "http://localhost:5000";
 
 export default axios.create({
